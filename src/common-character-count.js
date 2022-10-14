4693213f6a550328851780642db98558fcbc4e8a
Kanyshai Bakaeva <kbakaeva@MacBook-Pro-Kanyshai.local>
2022-10-14 16:21:31 +0600

    feat: task count-cat and common chcracter

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -12,8 +12,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let firstStr = s1.split('')
+  let secondStr = s2.split('')
+  let count = 0;
+  firstStr.sort((a, b) => a === b ? count += 1 : null)
+  secondStr.sort((a, b) => a === b ? count += 1 : null)
+  return count
 }
 
 module.exports = {
