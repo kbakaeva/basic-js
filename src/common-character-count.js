00b8342fa7328b64bf9370443ec82f4549936e42
kbakaeva <kanishay.bakaeva@gmail.com>
2022-10-14 18:48:37 +0600

    feat: add solutions

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -12,12 +12,12 @@ const { NotImplementedError } = require('../extensions/index.js');
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 function getCommonCharacterCount(s1, s2) {
-  let firstStr = s1.split('')
-  let secondStr = s2.split('')
-  let count = 0;
-  firstStr.sort((a, b) => a === b ? count += 1 : null)
-  secondStr.sort((a, b) => a === b ? count += 1 : null)
-  return count
+  // let firstStr = s1.split('')
+  // let secondStr = s2.split('')
+  // let count = 0;
+  // firstStr.sort((a, b) => a === b ? count += 1 : null)
+  // secondStr.sort((a, b) => a === b ? count += 1 : null)
+  // return count
 }
 
 module.exports = {
